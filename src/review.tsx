be8c04b7f54f8ad1f1374308b4d3e59c1caf8219
Sebastian Schloesser <s.schloesser@gmail.com>
2024-07-21 16:22:39 -0400

    Fix the review UI to be more clear

--- src/review.tsx
+++ src/review.tsx
@@ -24,7 +24,7 @@ const Review = ({ characters, trolleyPairings, decisions, aiDecisions }: ReviewP
         const savedIndex = decisions[i] ? pair[1] : pair[0];
         return characters[pair.find(index => index !== savedIndex)!];
     });
-    const text = showText ? 'What AI decided for you...' : 'Your conscious decisions';
+
     return (
         <Box sx={{ width: '100%', padding: 2 }}>
             {aiDecisions.length === 0 ? (
@@ -34,20 +34,27 @@ const Review = ({ characters, trolleyPairings, decisions, aiDecisions }: ReviewP
             ) : (
                 <>
                     <Typography variant="h3" align="center" sx={{ marginBottom: 2 }}>
-                        {text}
+                        Results
                     </Typography>
                     <Grid container spacing={2}>
                         <Grid item xs={12}>
-                            <Typography variant="h6">You saved</Typography>
+                            <Typography variant="h6">You Saved</Typography>
                         </Grid>
                         {savedCharacters.map((character, index) => (
-                            <Grid item xs key={index} wrap="nowrap">
-                                <Card sx={{ height: '100%', display: 'flex', flexDirection: 'column' }}>
+                            <Grid item xs key={index}>
+                                <Card
+                                    sx={{
+                                        height: '100%',
+                                        display: 'flex',
+                                        flexDirection: 'column',
+                                        border: !showText || (showText && !aiDecisions[index]) ? 'none':  '4px solid blue'
+                                    }}
+                                >
                                     <CardMedia
                                         component="img"
                                         image={character.img}
                                         alt={`Saved Character ${index + 1}`}
-                                        sx={{ height: 100, filter: !showText || (showText && aiDecisions[index]) ? 'none' : 'grayscale(100%)' }}
+                                        sx={{ height: 100 }}
                                     />
                                     <CardContent sx={{ flexGrow: 1 }}>
                                         <Typography variant="body2" align="center">
@@ -60,17 +67,20 @@ const Review = ({ characters, trolleyPairings, decisions, aiDecisions }: ReviewP
                             </Grid>
                         ))}
                         <Grid item xs={12}>
-                            <Divider sx={{ marginY: 2 }} />
+                            <Typography variant="h6">You Sacrificed</Typography>
                         </Grid>
-
                         {killedCharacters.map((character, index) => (
-                            <Grid item xs key={index} wrap="nowrap">
-                                <Card>
+                            <Grid item xs key={index}>
+                                <Card
+                                    sx={{
+                                        border: !showText || (showText && aiDecisions[index]) ? 'none' : '4px solid blue'
+                                    }}
+                                >
                                     <CardMedia
                                         component="img"
                                         image={character.img}
                                         alt={`Killed Character ${index + 1}`}
-                                        sx={{ height: 100, filter: !showText || (showText && aiDecisions[index]) ? 'grayscale(100%)' : 'none' }}
+                                        sx={{ height: 100, filter: 'grayscale(100%)' }}
                                     />
                                     <CardContent>
                                         <Typography variant="body2" align="center">
@@ -81,14 +91,15 @@ const Review = ({ characters, trolleyPairings, decisions, aiDecisions }: ReviewP
                                     </CardContent>
                                 </Card>
                             </Grid>
+
                         ))}
-                        <Grid item xs={12}>
-                            <Typography variant="h6">You sacrificed</Typography>
-                        </Grid>
+                        {showText && <Box sx={{ display: 'flex', alignItems: 'center', marginTop: 2, marginLeft: 2 }}>
+                            <Box sx={{ width: 20, height: 20, backgroundColor: 'blue', marginRight: 1 }} />
+                            <Typography sx={{ color: 'blue'  }}>
+                                = Who the AI decided to save on your behalf
+                            </Typography>
+                        </Box>}
                     </Grid>
-                    <Typography align="center" sx={{ marginTop: 20 }}>
-                        While you were observing the characters in the previous stage, and AI was trained to develop a bias based on these elections. Then it was asked to decide which characters to save at the same time you were deciding.
-                    </Typography>
                 </>
             )}
         </Box>
