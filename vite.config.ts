b3b145c22a558b8c0a5fd70ebab2e499cf086dc5
Sebastian Schloesser <s.schloesser@gmail.com>
2024-07-21 20:40:07 -0400

    Details to publish

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,18 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { resolve } from 'path';
 
 export default defineConfig({
   plugins: [react()],
   server: {
     open: true,
+  },
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        zine: resolve(__dirname, 'zine.html'),
+      },
+    },
   },
 });
