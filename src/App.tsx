6ea7685572e3372637d8ae7ca2ffccb977f41499
Sebastian Schloesser <s.schloesser@gmail.com>
2024-08-07 18:01:27 -0400

    Add individual components

--- src/App.tsx
+++ src/App.tsx
@@ -8,31 +8,81 @@ gsap.registerPlugin(useGSAP, ScrollTrigger, ScrollToPlugin);
 
 export default function Scroll() {
 
-  const menu = useRef()
+  const IntroPanel: React.FC = () => {
+    return (
+      <h1>Intro</h1>
+    )
+  }
+
+  const Panel1: React.FC = () => {
+    return (
+      <div>
+        <h1>Panel 1</h1>
+      </div>
+    )
+  }
+
+  const Panel3: React.FC = () => {
+    return (
+      <div>
+        <h1>Panel 3</h1>
+        <p>Subtitle 3</p>
+      </div>
+    )
+  }
+
+  const Panel4: React.FC = () => {
+    return (
+      <div>
+        <h1>Panel 4</h1>
+        <p>Subtitle 4</p>
+      </div>
+    )
+  }
+
+  const Panel6: React.FC = () => {
+    return (
+      <div>
+        <h1>Panel 6</h1>
+        <p>Subtitle 6</p>
+      </div>
+    )
+  }
+
+  const HPanel: React.FC<{ title: string }> = ({ title }) => {
+    return (
+      <div>
+        <h1>HORIZONTAL Panel {title} </h1>
+        <p>This is a custom panel component.</p>
+      </div>
+    )
+  }
 
   const panels = [
-    { title: 'Panel 1', subtitle: 'Subtitle 1', href: "#intro" },
+    { title: 'Panel 1', href: "#intro", component: IntroPanel },
     {
-      title: 'Panel 2', subtitle: 'Subtitle 2', href: "#panel2", horizontal: [
-        { title: 'H1', subtitle: 'Horizontal 1' },
-        { title: 'H2', subtitle: 'Horizontal 2' },
-        { title: 'H3', subtitle: 'Horizontal 3' },
-        { title: 'H4', subtitle: 'Horizontal 4' },
-        { title: 'H5', subtitle: 'Horizontal 5' },
+      title: 'Panel 2',
+      href: "#panel2",
+      horizontal: [
+        { title: 'H1', subtitle: 'Horizontal 1', component: HPanel },
+        { title: 'H2', subtitle: 'Horizontal 2', component: HPanel },
+        { title: 'H3', subtitle: 'Horizontal 3', component: HPanel },
+        { title: 'H4', subtitle: 'Horizontal 4', component: HPanel },
+        { title: 'H5', subtitle: 'Horizontal 5', component: HPanel },
       ]
     },
-    { title: 'Panel 3', subtitle: 'Subtitle 3', href: "#panel3" },
-    { title: 'Panel 4', subtitle: 'Subtitle 4', href: "#panel4" },
+    { title: 'Panel 3', subtitle: 'Subtitle 3', href: "#panel3", component: Panel3 },
+    { title: 'Panel 4', subtitle: 'Subtitle 4', href: "#panel4", component: Panel4 },
     {
       title: 'Panel 5', subtitle: 'Subtitle 5', href: "#panel5", horizontal: [
-        { title: 'H1', subtitle: 'Horizontal 1' },
-        { title: 'H2', subtitle: 'Horizontal 2' },
-        { title: 'H3', subtitle: 'Horizontal 3' },
-        { title: 'H4', subtitle: 'Horizontal 4' },
-        { title: 'H5', subtitle: 'Horizontal 5' },
+        { title: 'H1', subtitle: 'Horizontal 1', component: HPanel },
+        { title: 'H2', subtitle: 'Horizontal 2', component: HPanel },
+        { title: 'H3', subtitle: 'Horizontal 3', component: HPanel },
+        { title: 'H4', subtitle: 'Horizontal 4', component: HPanel },
+        { title: 'H5', subtitle: 'Horizontal 5', component: HPanel },
       ]
     },
-    { title: 'Panel 6', subtitle: 'Subtitle 6', href: "#panel6" },
+    { title: 'Panel 6', subtitle: 'Subtitle 6', href: "#panel6", component: Panel6 },
   ];
 
   useGSAP(
@@ -44,7 +94,7 @@ export default function Scroll() {
         anchor.addEventListener("click", function (e) {
           e.preventDefault();
           let targetElem = document.querySelector(e.target.getAttribute("href")),
-            y = targetElem    
+            y = targetElem
           gsap.to(window, {
             scrollTo: {
               y: y,
@@ -61,7 +111,7 @@ export default function Scroll() {
       horizontalContainers.forEach((container) => {
         const panels = gsap.utils.toArray(".panel", container)
         gsap.to(panels, {
-          xPercent: -100 * (panels.length),
+          xPercent: -100 * (panels.length - 1),
           scrollTrigger: {
             trigger: container,
             pin: true,
@@ -76,40 +126,43 @@ export default function Scroll() {
 
   return (
     <div>
-      <nav id="anchor-nav" ref={menu}>
+      <nav id="anchor-nav">
         {panels.map((panel, index) => (
-          <a href={panel.href} key={panel.title} className="anchor">
+          <a href={panel.href} key={panel.href} className="anchor">
             {panel.title}
           </a>
         ))}
       </nav>
 
       <div id="panels-container">
-        {panels.map((panel, index) => (
-          <section key={index} className="panel" id={panel.href.substring(1)}>
-            {!panel.horizontal && (
-              <div className="content">
-                <h1>{panel.title}</h1>
-                <h2>{panel.subtitle}</h2>
-              </div>
-            )}
-            {panel.horizontal && (
-              <div className="carousel" style={{ width: `${100 * panel.horizontal.length}%` }}>
-                <div className="horizontal-container">
-                  {panel.horizontal?.map((hPanel, hIndex) => (
-                    <article key={hIndex} className="panel">
-                      <div className="content">
-                        <h3>{hPanel.title}</h3>
-                        <h4>{hPanel.subtitle}</h4>
-                      </div>
-                    </article>
-                  ))}
+        {panels.map((panel, index) => {
+          const Component = panel.component;
+          return (
+            <section key={index} className="panel" id={panel.href.substring(1)}>
+              {!panel.horizontal && (
+                <div className="content">
+                  <Component />
                 </div>
-              </div>
-            )}
-          </section>
-
-        ))}
+              )}
+              {panel.horizontal && (
+                <div className="carousel" style={{ width: `${100 * panel.horizontal.length}%` }}>
+                  <div className="horizontal-container">
+                    {panel.horizontal?.map((hPanel, hIndex) => {
+                      const HComponent = hPanel.component;
+                      return (
+                        <article key={hIndex} className="panel">
+                          <div className="content">
+                            <HComponent title={hPanel.title}/>
+                          </div>
+                        </article>
+                      )
+                    })}
+                  </div>
+                </div>
+              )}
+            </section>
+          )
+        })}
       </div>
     </div>
   );
